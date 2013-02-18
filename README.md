6bbf3feb2e1806e5de4fb6ff68cc3290386f162e
Chad Norwood <chad@chadnorwood.com>
2013-02-18 09:48:56 -0600

    Work with ruby 1.8.7 (standard on mac), changed workflow and a few other things.

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 ruby-flickr-uploader
 ====================
 
-Flickr uploader is a ruby script to automatically upload,sort and tag photos to Flickr.
+Flickr uploader is a ruby script to automatically upload and tag photos to Flickr.
+
+It is designed to process many pictures and albums (sets), with a workflow that makes it easy to troubleshoot or see status just by looking at the filesystem. 
 
 ## Prerequisites
 
@@ -12,7 +14,10 @@ Flickr uploader is a ruby script to automatically upload,sort and tag photos to 
 ## Tested on
 
 * Mac OS X 10.7.3
+* Mac OS X 10.8
 * Ruby: ruby 1.9.1p431 (2011-02-18 revision 30908) [i386-darwin10.6.0]
+* Ruby: ruby 1.8.7
+
 
 ## Getting started
 
@@ -50,7 +55,31 @@ run `ruby authenticate.rb` to get your *access_token* and *access_secret*
 
 You can now add token (access_token) and (access_secret) secret to your *config.yml* 
 
+## Workflow
+
+The script's workflow, or process, is broken up to 3 steps, with 2 goals in mind. 
+The first goal is to make it fast and easy to get pictures onto flickr.  
+The second goal is to make it easy to troubleshoot if something goes wrong like internet connection breaks. 
+
+1. Look at albums in 'upload_path1_todo', where images go before starting upload
+
+2. As a picture is successfully uploaded, it is moved to 'upload_path2_inprogress'
+
+3. Once all images from an album in path1 are uploaded, album is removed from path1, and the path2 album 
+(which at this point is same as album was in path1 at the start) is moved to 'upload_path3_done'
+
+### Troubleshooting
+
+If internet connection does go out, or there is an issue with the flickr api, the above steps help you by 
+making it easy to compare the script thinks has happened with what you see on flickr.
+Specifically you can view which albums are done uploading, which are still in progress (and what images are left), and which have not even started.  
+On flickr you can search using tags to see which photos were actually uploaded successfully.
+
 ### Fetch your sets
+
+This script will not create sets if they do not exist. You can create your set using flickr api.
+
+If a set DOES exist, the photo will be added to it.  You should get names of photosets before uploading.
 
 Due to Flickr API limitation, I'm using a temporary yaml (*photosets.yml*) file to store your flickr sets. I use this file to query a set by its name.
 
@@ -60,30 +89,47 @@ Due to Flickr API limitation, I'm using a temporary yaml (*photosets.yml*) file 
 
 ### Upload process
 
-The script works with a hierarchy of two folder levels.
+The script works with a hierarchy of one or two folder levels.  For both levels, photos are given a tag that is the album name, 
+and a tag 'uploaded_by_rubyflickr' to make it easy to find and verify on flickr.com that the upload worked (this can be removed).
+If two folder levels, the folder under the album is used to give additional tags to photos within it.
+
+photos are added to album (set
+	* APP_CONFIG['upload_path']
+	  - Album1
+	    - IMG_0001.jpg
+	  - Album2
+	    - IMG_0002.jpg
+	  ...
 
     * APP_CONFIG['upload_path']
-      - Album1
+      - Album3
         - tag1
-          - IMG_0001.jpg
-      - Album2
+          - IMG_0003.jpg
+      - Album4
         - tag2,tag3
-          - IMG_0002.jpg
+          - IMG_0004.jpg
       ...
+
 
 In this example, the script will upload:
 
-* IMG_0001.jpg in album *Album1* with tag *tag1*
+* IMG_0001.jpg in album *Album1* with tags *Album1* and uploaded_by_rubyflickr
 
-* IMG_0002.jpg in album *Album2* with tags *tag2* and *tag3*
+* IMG_0002.jpg in album *Album2* with tags *Album2* and uploaded_by_rubyflickr
 
-Once a file is uploaded, it is *deleted* from the filesystem.
+* IMG_0003.jpg in album *Album3* with tags *Album3*, *tag1*, and uploaded_by_rubyflickr
+
+* IMG_0004.jpg in album *Album4* with tags *Album4*, *tag2*, *tag3*, and uploaded_by_rubyflickr
 
 
 ### Example
 
+    $ ruby get_photosets.rb 
+    You are now authenticated as chadn
+    photosets.yml saved.
+
     $ ruby upload.rb 
-    You are now authenticated as Campeur
+    You are now authenticated as chadn
     - will upload _MG_2070.jpg in album '2012 Oday' with tags ["Ilan"]
       found photoset with id: 72157629995407809
 	    upload done.
@@ -93,7 +139,6 @@ Once a file is uploaded, it is *deleted* from the filesystem.
 ## TODO
 
 * add a Gemfile
-* test on Ruby 1.8.x
 * add support for movie files
 * add pictures http://github.com/CharlyBr/ruby-flickr-uploader/raw/master/img/foo.png
 
--- upload.rb
+++ upload.rb
@@ -2,14 +2,16 @@
 # encoding: utf-8
 $LOAD_PATH << './'
 
-begin
-  found_gem = Gem::Specification.find_by_name('flickraw')
-rescue Gem::LoadError
-  puts "Could not find gem 'flickraw', try 'bundle install'"
-  exit
-end
+
+#begin
+#  found_gem = Gem::Specification.find_by_name('flickraw')
+#rescue Gem::LoadError
+#  puts "Could not find gem 'flickraw', try 'bundle install'"
+#  exit
+#end
 
 require 'rubygems'
+require 'FileUtils'
 require 'flickraw'
 require 'Photosets'
 
@@ -23,11 +25,8 @@ require 'Photosets'
 # http://www.flickr.com/services/api/upload.api.html
 
 APP_CONFIG = YAML.load_file("config.yml")['defaults']
-
-if APP_CONFIG['upload_path'].nil? or APP_CONFIG['upload_path'].empty? or !File.exists? APP_CONFIG['upload_path']
-  puts "Your config upload_path is empty or doesn't exist."
-  exit
-end
+$all_sets = Photosets.new
+$noUpload = false  # if true, don't actually upload to flickr - used for testing.
 
 FlickRaw.api_key = APP_CONFIG['api_key']
 FlickRaw.shared_secret = APP_CONFIG['shared_secret']
@@ -35,61 +34,160 @@ flickr.access_token = APP_CONFIG['access_token']
 flickr.access_secret = APP_CONFIG['access_secret']
 
 
-
-# From here you are logged:
-login = flickr.test.login
-puts "You are now authenticated as #{login.username}"
-
-all_sets = Photosets.new
-Dir.glob("#{APP_CONFIG['upload_path']}/*").each do |album|
-  next if album[0] == '.'
-  album_filename = File.basename album
-  #puts "album: #{album}"
-  #puts "album filename: #{album_filename}"
+def init
+  if not validateDir(APP_CONFIG['upload_path1_todo'],false)
+    puts "Your config upload_path1_todo doesn't exist."
+    exit
+  end
+  if APP_CONFIG['upload_path1_todo'].empty? 
+    puts "Your config upload_path1_todo is empty."
+    exit
+  end
+  t = Time.now   #=> 2007-11-17 15:18:03 +0900
+  puts "Processing #{APP_CONFIG['upload_path1_todo']} %s" % t.strftime("%FT%T")
   
-  Dir.glob("#{album}/*").each do |tags|
-    #puts "tags path: #{tags}"
-    tags_filename = File.basename tags
-    #puts "tags filename: #{tags_filename}"
-    next if tags_filename[0] == '.'
-    
-    Dir.glob("#{tags}/*").each do |picture|
+  login = flickr.test.login
+  puts "You are now authenticated as #{login.username}"
+  
+  Dir.glob("#{APP_CONFIG['upload_path1_todo']}/*").each do |album|
+    next if album[0] == '.'
+    album_filename = File.basename album
+    #puts "album: #{album}"
+    #puts "album filename: #{album_filename}"
 
-      picture_filename = File.basename picture
-      if not APP_CONFIG['allowed_ext'].include? File.extname(picture_filename)
-        puts "- #{File.extname(picture_filename)} are not allowed for upload, file was (#{picture_filename})"
-        next
-      end
-      
-      # exclude dotfiles
-      next if picture_filename[0] == '.'
+    if not validateDir "#{APP_CONFIG['upload_path2_inprogress']}/#{album_filename}", true
+      puts "Could not write to your config upload_path2_inprogress."
+      exit
+    end
 
-      puts "- will upload '#{picture_filename}' in album '#{album_filename}' with tags #{tags_filename.split(',')}"
-      
-      # Check if destination album exists
-      photoset = all_sets.get_set_by_title(album_filename)
-      if photoset == false
-        puts "\t couldn't find any photoset named #{album_filename}, aborting..."
-        next
+    # Check if destination album exists
+    photoset = $all_sets.get_set_by_title(album_filename)
+    if photoset == false
+      puts "\t Uploading album '#{album_filename}' as tagged pictures, no photoset found"
+    else
+      puts "\t Uploading album '#{album_filename}' to flickr photoset #{photoset['id']}"
+    end
+
+    Dir.glob("#{album}/*").each do |tags_or_picture|
+      #puts "tags path: #{tags}"
+      tags_or_picture_filename = File.basename tags_or_picture
+      #puts "tags_or_picture filename: #{tags_or_picture_filename}"
+      next if tags_or_picture_filename[0] == '.'
+
+
+      if File.directory?("#{tags_or_picture}")
+        if not validateDir "#{APP_CONFIG['upload_path2_inprogress']}/#{album_filename}/#{tags_or_picture_filename}", true
+          puts "Could not write to your config upload_path2_inprogress."
+          exit
+        end
+        Dir.glob("#{tags_or_picture}/*").each do |picture|
+          process_picture(album_filename, picture, tags_or_picture_filename)
+        end
+        if isEmpty tags_or_picture 
+          Dir.unlink tags_or_picture
+        end
       else
-        puts "\t found photoset with id: #{photoset['id']}"
+        process_picture(album_filename, tags_or_picture, '')
       end
-
-      picture_path = "#{picture}".encode("UTF-8")
-      encoded_tags = tags_filename.split(',').map{ |s| 
-        s.encode("UTF-8")
-        # add quotes for multiple words tags
-        %Q/"#{s}"/ 
-      }
-      picture_id = flickr.upload_photo picture_path, :title => picture_filename, :description => "", 
-                                            :tags =>encoded_tags.join(' '), :is_public => APP_CONFIG['is_public']
-      if picture_id
-        puts "\t upload done."
-        File.unlink picture_path
-        puts "\t file picture deleted."
-        puts "\t adding picture to set #{photoset['id']}"
-        res = flickr.call "flickr.photosets.addPhoto", {'photoset_id' => photoset['id'], 'photo_id' => picture_id}
-      end
+    end
+    if isEmpty album 
+      Dir.unlink album  # in path1
+      oldname = "#{APP_CONFIG['upload_path2_inprogress']}/#{album_filename}"
+      newname = "#{APP_CONFIG['upload_path3_done']}/#{album_filename}"
+      File.rename oldname, newname
+      puts "Done with album: #{album}\n\n"
+    else
+      puts "NOT Done with album: #{album}\n\n"
     end
   end
 end
+
+
+
+def process_picture album_filename, picture, tags_filename
+  picture_filename = File.basename picture
+  if not APP_CONFIG['allowed_ext'].include? File.extname(picture_filename)
+    puts "- #{File.extname(picture_filename)} are not allowed for upload, file was (#{picture_filename})"
+    return
+  end
+  
+  # exclude dotfiles
+  return if picture_filename[0] == '.'
+
+  if not tags_filename
+    tags_filename += ','
+  end
+  tags_filename += "#{album_filename}"
+  tags_filename += ",uploaded_by_rubyflickr"  # comment this out if necessary.
+  puts "- will upload '#{picture_filename}' in album '#{album_filename}' with tags: #{tags_filename}"
+
+  begin
+    # NOTE: encode not supported in ruby 1.8.7, but is in ruby  1.9.x
+    picture_path = "#{picture}".encode("UTF-8") 
+  rescue
+    #puts "#{$!}"
+    picture_path = "#{picture}"
+  end
+  encoded_tags = tags_filename.split(',').map{ |s| 
+    # add quotes for multiple words tags
+    begin
+      s.encode("UTF-8")
+    rescue
+      s
+    end
+    %Q/"#{s}"/ 
+  }
+  if $noUpload
+    picture_id = 1212
+    sleep 2
+  else
+    picture_id = flickr.upload_photo picture_path, :title => picture_filename, :description => "", 
+                               :tags =>encoded_tags.join(' '), :is_public => APP_CONFIG['is_public']
+  end
+  
+  if not picture_id
+    puts "\t upload failed. '#{picture_filename}' in album '#{album_filename}' with tags #{tags_filename}"
+    return
+  end
+  
+  # Success! Now move pic from path1 to path2
+  r = Regexp.new(APP_CONFIG['upload_path1_todo'])
+  newpicture = picture_path.gsub(r, APP_CONFIG['upload_path2_inprogress'])
+  File.rename  picture_path, newpicture
+  puts "\t upload done. moved to #{newpicture}"
+  
+  # add to album if it exists
+  photoset = $all_sets.get_set_by_title(album_filename)
+  if photoset 
+    puts "\t Adding pic to album '#{album_filename}' photoset #{photoset['id']}"
+    if not $noUpload
+      res = flickr.call "flickr.photosets.addPhoto", {'photoset_id' => photoset['id'], 'photo_id' => picture_id}
+    end
+  end
+end
+
+def validateDir path, create
+  if not create
+    return !(path.nil? or !File.exists? path)
+  else
+    if not File.exist? path
+      FileUtils.mkdir_p path
+    else
+      return File.directory? path
+    end
+  end
+end
+
+def isEmpty path
+  Dir.glob("#{path}/*").each do |fn|
+    fn_base = File.basename fn
+    next if fn_base == '.'
+    next if fn_base == '..'
+    #puts "is not empty: #{path} -- #{fn_base}"
+    return false
+  end
+  #puts "is empty: #{path}"
+  return true
+end
+
+init
